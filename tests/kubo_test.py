ef696aaaebc48da706e9dff62f60619bc5f768af
teichen <paul.teichen@gmail.com>
2023-01-24 10:36:15 -0800

    disentangle unit tests

--- tests/kubo_test.py
+++ tests/kubo_test.py
@@ -17,43 +17,17 @@ class KuboTester(unittest.TestCase):
         self.ntraj = 100 # 1000  # number of stochastic trajectories
         self.t     = np.linspace(0, self.npts * self.dt, self.npts) # time vector
 
+        self.w0   = 1                 # frequencies measured in units of w0
+        self.zet  = 0.05              # damping ratio
+        self.g0   = 1 / pi            # covariance of white noise is zero mean, pi*g0*dt variance
+        self.gam  = 2 * self.zet * self.w0
+        self.temp = pi * self.g0 / (2*self.gam) # temperature
+
     def test_finite_batches(self):
         """
         """
-        # Benchmark of Fig. 4.3 M. Grigoriu ``Applied Non-Gaussian Processes"
-
-        w0   = 1                 # frequencies measured in units of w0
-        zet  = 0.05              # damping ratio
-        g0   = 1 / pi            # covariance of white noise is zero mean, pi*g0*dt variance
-        gam  = 2 * zet * w0
-        temp = pi * g0 / (2*gam) # temperature
-
-        x0 = sqrt(pi * g0 / (4*zet*pow(w0, 3))) * random.rand(self.ntraj, 1)
-        p0 = sqrt(pi * g0 / (4*zet*w0)) * random.rand(self.ntraj, 1)
-
-        x = np.zeros((self.ntraj, self.npts))
-        p = np.zeros((self.ntraj, self.npts))
-
-        for ii in range(self.ntraj):
-            x[ii, 0] = 0 # initial conditions
-            p[ii, 0] = 0
-            
-            fpts = sqrt(self.dt*pi*g0) * random.rand(self.npts, 1) # trajectory of noise 
-            for jj in range(1, self.npts):
-                x[ii, jj] = x[ii, jj-1] + p[ii, jj-1] * self.dt
-                p[ii, jj] = p[ii, jj-1] * (1-2*zet*w0*self.dt) - (w0 ** 2) * x[ii, jj-1] * self.dt + fpts[jj-1]
-
-        x2 = np.zeros((self.npts, 0)) # mean of the squared displacement
-        for jj in range(self.npts):
-            mux    = np.mean(x[:, jj])
-            x2[jj] = np.mean((x[:, jj] - mux) ** 2) 
-
-        kubo = sqrt(g0 / (2 * gam))
+        kubo = sqrt(self.g0 / (2 * self.gam))
         x, cx, g_traj = Intensity(kubo)
-        k  = 10
-        dt = 0.0001 # kap = 10
-
-        # kubo = exp(-(k^2)*(exp(-t)-1+t))
 
         # Estimate the standard deviation from the mean when samples are drawn 10 at a time.
 
@@ -64,10 +38,12 @@ class KuboTester(unittest.TestCase):
 
         # TODO: assert dynamics independent of batch size
 
-        # Benchmark of Sec. 1.3 from R. Zwanzig's ``Nonequilibrium Statistical Mechanics"
 
-        x0 = sqrt(pi*g0/(4*zet*pow(w0, 3)))*random.rand(self.ntraj, 1)
-        p0 = sqrt(pi*g0/(4*zet*w0))*random.rand(self.ntraj, 1)
+    def test_zwanzig(self):
+        """ Benchmark of Sec. 1.3 from R. Zwanzig's ``Nonequilibrium Statistical Mechanics"
+        """
+        x0 = sqrt(pi*self.g0/(4*self.zet*pow(self.w0, 3)))*random.rand(self.ntraj, 1)
+        p0 = sqrt(pi*self.g0/(4*self.zet*self.w0))*random.rand(self.ntraj, 1)
 
         x = np.zeros((self.ntraj, self.npts))
         p = np.zeros((self.ntraj, self.npts))
@@ -76,17 +52,43 @@ class KuboTester(unittest.TestCase):
             x[ii, 0] = 0 # initial conditions
             p[ii, 0] = 0
             
-            fpts = sqrt(dt*pi*g0) * random.rand(self.npts, 1) # trajectory of noise 
+            fpts = sqrt(self.dt*pi*self.g0) * random.rand(self.npts, 1) # trajectory of noise 
             for jj in range(1, self.npts):
                 x[ii, jj] = x[ii, jj-1] + p[ii, jj-1] * self.dt
 
                 # Grigoriu benchmark excluding the (-(w0 ** 2) * x[ii, jj-1] * dt) term
-                p[ii, jj] = p[ii, jj-1] * (1-2*zet*w0*self.dt) + fpts[jj-1] 
+                p[ii, jj] = p[ii, jj-1] * (1-2*self.zet*self.w0*self.dt) + fpts[jj-1] 
 
         x2 = np.zeros((self.npts, 1)) # mean of the squared displacement
         for jj in range(self.npts):
             mux    = np.mean(x[:, jj])
             x2[jj] = np.mean((x[:, jj] - mux) ** 2)
+
+        # TODO: add asserts
+
+
+    def test_finite_batches_grigoriu(self):
+        """ Benchmark of Fig. 4.3 M. Grigoriu ``Applied Non-Gaussian Processes"
+        """
+        x0 = sqrt(pi * self.g0 / (4*self.zet*pow(self.w0, 3))) * random.rand(self.ntraj, 1)
+        p0 = sqrt(pi * self.g0 / (4*self.zet*self.w0)) * random.rand(self.ntraj, 1)
+
+        x = np.zeros((self.ntraj, self.npts))
+        p = np.zeros((self.ntraj, self.npts))
+
+        for ii in range(self.ntraj):
+            x[ii, 0] = 0 # initial conditions
+            p[ii, 0] = 0
+            
+            fpts = sqrt(self.dt*pi*self.g0) * random.rand(self.npts, 1) # trajectory of noise 
+            for jj in range(1, self.npts):
+                x[ii, jj] = x[ii, jj-1] + p[ii, jj-1] * self.dt
+                p[ii, jj] = p[ii, jj-1] * (1-2*self.zet*self.w0*self.dt) - (self.w0 ** 2) * x[ii, jj-1] * self.dt + fpts[jj-1]
+
+        x2 = np.zeros((self.npts, 0)) # mean of the squared displacement
+        for jj in range(self.npts):
+            mux    = np.mean(x[:, jj])
+            x2[jj] = np.mean((x[:, jj] - mux) ** 2) 
 
         # TODO: add asserts
 
